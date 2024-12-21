5598de572e5a333fe65cb29af48abefd886631da
sabhireddy05091999 <bsanthosh495@gmail.com>
2024-12-21 23:39:21 +0000

    utils added

--- template.py
+++ template.py
@@ -7,18 +7,24 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "wineproject"
 
+
+
 list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "dvc.yaml",
     "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
@@ -31,8 +37,8 @@ list_of_files = [
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
 
+    filedir, filename = os.path.split(filepath)
 
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
