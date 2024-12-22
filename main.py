1795b1ecb93c3fd1ce4454f030e8772b5aff1890
sabhireddy05091999 <bsanthosh495@gmail.com>
2024-12-22 06:53:52 +0000

    data transformation done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from wineproject import logger
 from wineproject.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from wineproject.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
-
+from wineproject.pipeline.stage_3_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -17,6 +17,16 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
